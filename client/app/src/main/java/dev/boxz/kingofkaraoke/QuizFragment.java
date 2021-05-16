8d1949e934f02e1817599be1b9ebde50d45bab0c
boxmars <2803726925@qq.com>
2021-05-17 07:51:52 +0800

    Update

--- client/app/src/main/java/dev/boxz/kingofkaraoke/QuizFragment.java
+++ client/app/src/main/java/dev/boxz/kingofkaraoke/QuizFragment.java
@@ -1,5 +1,6 @@
 package dev.boxz.kingofkaraoke;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class QuizFragment extends Fragment {
@@ -39,7 +43,12 @@ public class QuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_quiz, container, false);
-
+        TextView questionTextView=view.findViewById(R.id.questionTextView);
+        SeekBar seekBar=view.findViewById(R.id.seekBar);
+        Button startBtn=view.findViewById(R.id.startMusicBtn);
+        Button stopBtn=view.findViewById(R.id.stopMusicBtn);
+        
+        questionTextView.setText("Who is the singer?");
 
 
         return view;
