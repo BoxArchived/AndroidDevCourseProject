f0d93b538f87d8655d2bacbba94967a0f045e5be
boxmars <2803726925@qq.com>
2021-05-18 05:22:26 +0800

    Update QuizActivity.java

--- client/app/src/main/java/dev/boxz/kingofkaraoke/QuizActivity.java
+++ client/app/src/main/java/dev/boxz/kingofkaraoke/QuizActivity.java
@@ -17,12 +17,11 @@ import okhttp3.OkHttpClient;
 
 public class QuizActivity extends AppCompatActivity {
     final String  API_URL="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        Intent intent=new Intent(this,UpdateActivity.class);
-        startActivity(intent);
         Button previousBtn=findViewById(R.id.previousBtn);
         Button nextBtn=findViewById(R.id.nextBtn);
         Button submitBtn=findViewById(R.id.submitBtn);
