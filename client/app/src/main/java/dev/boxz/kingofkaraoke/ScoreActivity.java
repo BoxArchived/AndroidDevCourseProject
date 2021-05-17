4aedd5045885034a7e79a24f5b38f7cb43240b1a
boxmars <2803726925@qq.com>
2021-05-17 09:18:27 +0800

    Create ScoreActivity.java

---
+++ client/app/src/main/java/dev/boxz/kingofkaraoke/ScoreActivity.java
@@ -0,0 +1,16 @@
+package dev.boxz.kingofkaraoke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+    }
+}
