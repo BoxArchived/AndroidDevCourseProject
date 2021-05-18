35c8a3bdd94bdb881d26597dd2080f9300e48625
boxmars <2803726925@qq.com>
2021-05-18 18:33:58 +0800

    Update QuizFragment.java

--- client/app/src/main/java/dev/boxz/kingofkaraoke/QuizFragment.java
+++ client/app/src/main/java/dev/boxz/kingofkaraoke/QuizFragment.java
@@ -26,9 +26,11 @@ import java.io.File;
 public class QuizFragment extends Fragment {
     private static final String QUESTION = "QUESTION";
     MediaPlayer mediaPlayer;
-
+    Button play;
+    Button stop;
+    Button pause;
     private Question mQuestion;
-
+    SeekBar seekBar;
     public QuizFragment() {
     }
 
@@ -54,10 +56,10 @@ public class QuizFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_quiz, container, false);
         TextView questionTextView=view.findViewById(R.id.questionTextView);
-        SeekBar seekBar=view.findViewById(R.id.seekBar);
-        Button play=view.findViewById(R.id.startMusicBtn);
-        Button stop=view.findViewById(R.id.stopMusicBtn);
-        Button pause=view.findViewById(R.id.pauseMusicBtn);
+        seekBar=view.findViewById(R.id.seekBar);
+        play=view.findViewById(R.id.startMusicBtn);
+        stop=view.findViewById(R.id.stopMusicBtn);
+        pause=view.findViewById(R.id.pauseMusicBtn);
         ImageView imageView=view.findViewById(R.id.coverImage);
         RadioGroup radioGroup=view.findViewById(R.id.radioGroup);
         RadioButton radioButtonA=view.findViewById(R.id.optionA);
@@ -79,30 +81,31 @@ public class QuizFragment extends Fragment {
                 }
             }
         };
+        mediaPlayer=MediaPlayer.create(getActivity().getApplicationContext(), Uri.fromFile(new File(getActivity().getFilesDir(),mQuestion.getFilePath())));
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mediaPlayer==null){
                     mediaPlayer=MediaPlayer.create(getActivity().getApplicationContext(), Uri.fromFile(new File(getActivity().getFilesDir(),mQuestion.getFilePath())));
-                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            mediaPlayer.stop();
-                            mediaPlayer.release();
-                            mediaPlayer=null;
-                            seekBar.setProgress(0);
-                            play.setEnabled(true);
-                            stop.setEnabled(false);
-                            pause.setEnabled(false);
-                        }
-                    });
-                    mediaPlayer.start();
-                    play.setEnabled(false);
-                    stop.setEnabled(true);
-                    pause.setEnabled(true);
-                    seekBar.setMax(mediaPlayer.getDuration());
-                    handler.postDelayed(runnable,200);
                 }
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                        mediaPlayer=null;
+                        seekBar.setProgress(0);
+                        play.setEnabled(true);
+                        stop.setEnabled(false);
+                        pause.setEnabled(false);
+                    }
+                });
+                mediaPlayer.start();
+                play.setEnabled(false);
+                stop.setEnabled(true);
+                pause.setEnabled(true);
+                seekBar.setMax(mediaPlayer.getDuration());
+                handler.postDelayed(runnable,200);
             }
         });
         pause.setOnClickListener(new View.OnClickListener() {
@@ -165,5 +168,33 @@ public class QuizFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mediaPlayer==null){
+            mediaPlayer=MediaPlayer.create(getActivity().getApplicationContext(), Uri.fromFile(new File(getActivity().getFilesDir(),mQuestion.getFilePath())));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer=null;
+        play.setEnabled(true);
+        pause.setEnabled(false);
+        stop.setEnabled(false);
+        seekBar.setProgress(0);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mediaPlayer=null;
+        play.setEnabled(true);
+        pause.setEnabled(false);
+        stop.setEnabled(false);
     }
 }
