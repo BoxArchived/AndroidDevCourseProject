1b433a21089f58a651e129b4f24ad5bb8ab7b88e
boxmars <2803726925@qq.com>
2021-05-18 18:34:54 +0800

    Update urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('version', views.get_version),
     path('songs',views.get_songs),
     path('questions',views.get_questions),
-    # path('',views.test)
+    path('getrank',views.get_rank),
+    path('submitrank',views.submit_rank),
 ]
