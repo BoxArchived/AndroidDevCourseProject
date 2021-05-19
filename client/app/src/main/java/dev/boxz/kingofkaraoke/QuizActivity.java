891d5aa8212d5eb35c1f1cf101c99650fbd2d71b
boxmars <2803726925@qq.com>
2021-05-19 23:30:55 +0800

    Update QuizActivity.java

--- client/app/src/main/java/dev/boxz/kingofkaraoke/QuizActivity.java
+++ client/app/src/main/java/dev/boxz/kingofkaraoke/QuizActivity.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -16,12 +18,11 @@ import java.util.ArrayList;
 import okhttp3.OkHttpClient;
 
 public class QuizActivity extends AppCompatActivity {
-
+    public static CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
         ViewPager2 viewPager2=findViewById(R.id.viewPage);
 
         QuizAdapter quizAdapter=new QuizAdapter(QuizActivity.this,Question.questionArrayList);
@@ -33,10 +34,31 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         ViewPager2 viewPager2=findViewById(R.id.viewPage);
-
         QuizAdapter quizAdapter=new QuizAdapter(QuizActivity.this,Question.questionArrayList);
         viewPager2.setAdapter(quizAdapter);
+        countDownTimer=new CountDownTimer(300000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                TextView textView=findViewById(R.id.timerTextView);
+                textView.setText(millisUntilFinished/1000+"s/300s");
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent1=new Intent();
+                intent1.setClass(getApplicationContext(),ScoreActivity.class);
+                startActivity(intent1);
+            }
+        };
+        countDownTimer.start();
         super.onResume();
 
+    }
+
+    @Override
+    protected void onPause() {
+        countDownTimer.cancel();
+        countDownTimer=null;
+        super.onPause();
     }
 }
